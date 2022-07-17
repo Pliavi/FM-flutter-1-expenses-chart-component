64502be5c7e3c46bcd5561f212d53b06d4fef5c1
Pliavi <pliavi.rpgm@gmail.com>
2022-07-17 18:10:12 -0300

    add colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CColors {
+  static const Color primary = Color(0xffeb755d);
+  static const Color secondary = Color(0xff76b5bc);
+
+  static const Color background = Color(0xfff7e9dc);
+  static const Color borders = Color(0xfff7e9dc);
+
+  static const Color darkBrown = Color(0xff392618);
+  static const Color mediumBrown = Color(0xff93867b);
+  static const Color cream = Color(0xfff8e9dd);
+  static const Color veryPaleOrange = Color(0xfffffaf5);
+}
