a7b49e0e0f1bfaec7b072ee486c9fda11c9ca48f
Pliavi <pliavi.rpgm@gmail.com>
2022-07-17 22:37:14 -0300

    take data from json

--- lib/components/expenses_chart.dart
+++ lib/components/expenses_chart.dart
@@ -1,15 +1,37 @@
+import 'dart:convert';
+
 import 'package:expense_chart_component/components/my_balance_card.dart';
 import 'package:expense_chart_component/components/spending_summary.dart';
 import 'package:expense_chart_component/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ExpenseChart extends StatelessWidget {
+class ExpenseChart extends StatefulWidget {
   const ExpenseChart({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
+  State<ExpenseChart> createState() => _ExpenseChartState();
+}
 
+class _ExpenseChartState extends State<ExpenseChart> {
+  Map<String, double> _spendings = {
+    'mon': 0.0,
+    'tue': 0.0,
+    'wed': 0.0,
+    'thu': 0.0,
+    'fri': 0.0,
+    'sat': 0.0,
+    'sun': 0.0,
+  };
+
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CColors.background,
       body: Center(
@@ -20,14 +42,31 @@ class ExpenseChart extends StatelessWidget {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                MyBalanceCard(textTheme: textTheme),
+                const MyBalanceCard(),
                 const SizedBox(height: 16),
-                SpendingSummary(textTheme: textTheme)
+                SpendingSummary(spendings: _spendings),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void fetchData() async {
+    const String jsonUrl = 'assets/data/data.json';
+    final String jsonString = await rootBundle.loadString(jsonUrl);
+    final List<dynamic> data = await json.decode(jsonString);
+    final Map<String, double> newData = {};
+
+    for (final spending in data) {
+      final day = spending['day'];
+      final amount = spending['amount'];
+
+      newData[day] = amount;
+    }
+
+    _spendings = newData;
+    setState(() {});
   }
 }
